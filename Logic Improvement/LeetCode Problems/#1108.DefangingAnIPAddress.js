682ab6ecbc414a0da2026d76fdaf326fd7a1083d
ShuvashishSQA <shuvashishsqa@gmail.com>
2026-07-12 17:58:06 +0600

    Solving 1108. Defanging an IP Address

---
+++ Logic Improvement/LeetCode Problems/#1108.DefangingAnIPAddress.js
@@ -0,0 +1,17 @@
+function defangingIP(ipAddress){
+    let defangedIP = "";
+
+    for (let i = 0; i <ipAddress.length; i++){
+        if (ipAddress[i] === "."){
+            defangedIP += "[.]";
+            }
+
+        else{
+            defangedIP += ipAddress[i];
+           }
+    }
+
+return defangedIP;
+}
+
+console.log(defangingIP("1.1.1.1"));
