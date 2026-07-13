28b7b92596ffea7d36d9484ffeaa766bb1ab61e2
shuvashishSQA <shuvashishsqa@gmail.com>
2026-07-14 00:31:44 +0600

    solving the DefangingIPAddress problem using regular loop if else, split+join and replaceAll

--- Logic Improvement/LeetCode Problems/#1108.DefangingAnIPAddress.js
+++ Logic Improvement/LeetCode Problems/#1108.DefangingAnIPAddress.js
@@ -1,17 +1,31 @@
+/**
+ * @param {string} ipAddress
+ */
+
 function defangingIP(ipAddress){
     let defangedIP = "";
 
     for (let i = 0; i <ipAddress.length; i++){
+        
         if (ipAddress[i] === "."){
-            defangedIP += "[.]";
-            }
-
+            defangedIP += "[.]";}
         else{
-            defangedIP += ipAddress[i];
-           }
+            defangedIP += ipAddress[i];}
     }
-
 return defangedIP;
 }
+console.log(defangingIP('1.1.1.1'));
 
-console.log(defangingIP("1.1.1.1"));
+
+//using split and join 
+function defangingIP2(ipAddress){
+    return ipAddress.split(".").join("[.]");
+}
+console.log(defangingIP2("1.1.1.1"));
+
+
+//using replaceAll
+function defangingIP3(ipAddress){
+    return ipAddress.replaceAll( ".", "[.]");
+}
+console.log(defangingIP3("1.1.1.1"));
